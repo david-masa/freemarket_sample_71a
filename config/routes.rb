d1c301b566dd1731acc85027296491636c12c334
TakeyukiShimizu <take54883@gmail.com>
2020-06-20 23:51:23 +0900

    各ページ遷移の為のパス設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,20 @@
 Rails.application.routes.draw do
-  root 'top#index'
+  root 'users#index'
+
+  resources :posts, only: :index
+
+  resources :newreg, only: :index
+
+  resources :log, only: :index
+
+  resources :edit, only: :index
+
+  resources :purchase, only: :index
+
+  resources :users, only: :index
+
+  resources :logout, only: :index
+
+  resources :cards, only: [:index, :new]
+
+end
