1cad1ec331cc1d37345530b4b4c723008efc4928
TakeyukiShimizu <take54883@gmail.com>
2020-06-26 23:20:09 +0900

    マイページ、商品購入確認のビューを仮表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_185109) do
+ActiveRecord::Schema.define(version: 2020_06_23_145807) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
