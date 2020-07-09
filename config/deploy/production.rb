eb1a0368e93ed38ca802dac2331d37c9bf482800
furufurus <s.furu444@ozzio.jp>
2020-07-09 21:21:35 +0900

    再デプロイ2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.178.189.238', user: 'ec2-user', roles: %w{app db web}
+server '18.181.155.100', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
