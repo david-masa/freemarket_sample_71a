50bdfedcd8a8088212c97c9363ba95b03e098b96
david-masa <60571920+david-masa@users.noreply.github.com>
2020-07-15 22:31:30 +0900

    ミス直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_27_101545) do
-
+ActiveRecord::Schema.define(version: 2020_07_06_130244) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
@@ -39,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_06_27_101545) do
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "conditions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 2020_06_27_101545) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
     t.string "name", null: false
     t.text "detail", null: false
     t.integer "price", null: false
@@ -75,7 +74,6 @@ ActiveRecord::Schema.define(version: 2020_06_27_101545) do
     t.integer "shipping_days", null: false
     t.integer "costomer"
     t.integer "shipping_area_id"
-    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -102,5 +100,4 @@ ActiveRecord::Schema.define(version: 2020_06_27_101545) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "images", "items"
-  add_foreign_key "items", "users"
 end
