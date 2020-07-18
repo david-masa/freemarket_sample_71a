e8afd2c137ad1d0076adbe407c74bea2bc0e26a8
david-masa <60571920+david-masa@users.noreply.github.com>
2020-07-18 09:40:12 +0900

    商品編集機能実装（仮）

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
   def index
 
-    @items = Item.includes(:images).order('created_at DESC').page(params[:page]).per(3)
+    @items = Item.includes(:images).order('created_at DESC').page(params[:page]).per(5)
     
     @category_parent_array = Category.where(ancestry: nil)
 
