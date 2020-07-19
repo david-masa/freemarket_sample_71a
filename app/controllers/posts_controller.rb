66e7ed071b11c72258e001076007ebcd29468e6e
takeyukigoto <62117691+takeyukigoto@users.noreply.github.com>
2020-07-20 08:27:36 +0900

    add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,24 @@
 class PostsController < ApplicationController
   def index
-    @posts = Item.all
+    @item = Item.all
+    
+    
   end
+
+  def show
+    @item = Item.includes(:images)
+    @item = Item.find(params[:id])
+    
+  end
+
+
+
 
   def edit
   end
 
+
+  
   
 
 end
