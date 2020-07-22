cf41aa172f303d14bef05c45c1a316b4437523e9
TakeyukiShimizu <take54883@gmail.com>
2020-07-22 12:18:04 +0900

    クレジットカード登録ができない修正

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,7 +1,7 @@
 document.addEventListener(
   "DOMContentLoaded", e => {
     if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
-      Payjp.setPublicKey("pk_test_61fe8dd669a59526fd8542f5"); //ここに公開鍵を直書き
+      Payjp.setPublicKey("pk_test_6afea2b0dca474896c17a887"); //ここに公開鍵を直書き
       let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得されます
       btn.addEventListener("click", e => { //ボタンが押されたときに作動します
         e.preventDefault(); //ボタンを一旦無効化します
--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,9 @@ ActiveRecord::Schema.define(version: 2020_07_19_212333) do
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "token", null: false
     t.bigint "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
@@ -103,6 +104,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_212333) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "cards", "users"
   add_foreign_key "images", "items"
   add_foreign_key "items", "users"
 end
