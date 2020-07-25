b1032a8e8c2525709fd6d608cdc729e2e5a41f57
furufurus <s.furu444@ozzio.jp>
2020-07-25 12:42:06 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,7 @@
 class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-
-  validates :name, :detail, :price, :condition_id, :shipping_area_id, :shipping_days, :shipping_cost, :category_id, presence: true  validates :user_id, :name, :detail, :price, :condition_id, :shipping_area_id, :shipping_days, :shipping_cost, :category_id, presence: true
-
+  validates :name, :detail, :price, :condition_id, :shipping_area_id, :shipping_days, :shipping_cost, :category_id, presence: true
   validates :images, presence: true
   validates :price, :numericality => { :greater_than => 299 }
   validates :price, :numericality => { :less_than => 9999999  }
